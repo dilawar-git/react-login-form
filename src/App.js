d7e90d85d935ded3e1c815254812be0c832dd3ef
dilawar-git <72309113+dilawar-git@users.noreply.github.com>
2020-10-17 00:56:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Login from './Login';
+import Register from './Register';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+
+function App() {
+  return (
+    <div className="login-page">
+    <Tabs >
+    <TabList style={{"list-style":"none","display":"inline-flex","padding-left":"0px"}} selected="">
+      <Tab style={{"padding":"5px"}} className="tab-st">Login</Tab>
+      <Tab style={{"padding":"5px"}} className="tab-st">Register</Tab>
+    </TabList>
+ 
+    <TabPanel>
+      <Login />
+    </TabPanel>
+    <TabPanel>
+      <Register />
+    </TabPanel>
+  </Tabs>
+  </div>
+    
+  );
+}
+
+export default App;
